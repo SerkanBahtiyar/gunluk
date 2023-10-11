17072ec772ee5f3d8e0c5b19ece4e231b2d65904
Serkan <68036775+SerkanBahtiyar@users.noreply.github.com>
2023-10-12 02:02:06 +0300

    gunluk calismasi

---
+++ gunluk/gunluk/Controllers/GunlukController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using gunluk.Models.Entity;
+namespace gunluk.Controllers
+{
+    public class GunlukController : Controller
+    {
+        // GET: Gunluk
+        gunlukEntities sb=new gunlukEntities();
+        public ActionResult Index()
+        {
+            var degerler = sb.gun.ToList();
+            return View(degerler);
+        }
+        public ActionResult bilgiTecrubeGetir()
+        {
+            var degerler = sb.gun.ToList();
+            return View(degerler);
+        }
+        [HttpGet]
+        public ActionResult gunlukEkle()
+        {
+            return View();
+        }
+
+      
+        [HttpPost]
+        public ActionResult gunlukEkle(gun b)
+        {
+            sb.gun.Add(b);
+            sb.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public ActionResult tecrubeEkle()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public ActionResult tecrubeEkle(gun b)
+        {
+            sb.gun.Add(b);
+            sb.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
